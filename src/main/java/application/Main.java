6bae52288a357012d09363292386ac48d56dc456
victor16092006 <271762@unifio.edu.br>
2025-09-10 01:33:46 +0000

    aula_09/09

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,7 +2,11 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+      //Declaração de Vetores
+        int[] numeros = new int[5];
+
+      //Declaração de Matrizes
+       int [] [] matriz = new int[5][3];
     }
 }
 
